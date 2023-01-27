b0726b6a2584942df7abd40d4ed56d85a9c9c93d
gve-ableton <giacomo.vergnano@gmail.com>
2023-01-27 13:45:26 +0100

    fix node version

---
+++ __test__/testUrlChecker.spec.js
@@ -0,0 +1,7 @@
+
+import { checkForURL } from "../src/client/js/urlChecker"
+
+describe("Testing the submit functionality", () => {
+    test("Testing the checkForURL() function", () => {
+           expect(checkForURL).toBeDefined();
+})});
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
@@ -14,6 +19,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -30,6 +39,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
