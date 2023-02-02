94f82a70b6e31570eeac52c77b664fad85db00fd
gve-ableton <giacomo.vergnano@gmail.com>
2023-02-02 10:56:59 +0100

    reset project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Requirements
-I had a lot of issues with dependencies and the I finally found the reason being the Node version.
-The required node version should be v14.0.0 and consequent npm version of v6.14.4.
--- __test__/testUrlChecker.spec.js
+++
@@ -1,7 +0,0 @@
-
-import { checkForURL } from "../src/client/js/urlChecker"
-
-describe("Testing the submit functionality", () => {
-    test("Testing the checkForURL() function", () => {
-           expect(checkForURL).toBeDefined();
-})});
